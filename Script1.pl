6e1c31cb8712edc08953c1ccbbcbc0536fb1edfd
Serena15 <raketa9390@mail.ru>
2015-10-07 15:20:33 +0300

    Update Script1.pl

--- Script1.pl
+++ Script1.pl
@@ -1,1 +1,1 @@
-dir /b | perl -ln -e "{ BEGIN{ $count1=0;$count2=0; }{ if(-s $_ > 1048576){ print $_; $count2++; }$count1++; }END{ print 'All files: '.$count1.'; Files with size > 1Mb: '.$count2; } }"
+dir /b | perl -ln -e "{ BEGIN{ $count1=0; $count2=0; }{ if(-s $_ > 1048576){ print $_; $count2++; } $count1++; }END{ print 'All files: '.$count1.'; Files with size > 1Mb: '.$count2; } }"
